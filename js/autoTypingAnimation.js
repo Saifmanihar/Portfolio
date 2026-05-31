a7349321700ff17ac83679500dee7d57d695670e
saif_manihar <96367331+Saifmanihar@users.noreply.github.com>
2026-05-31 15:36:45 +0530

    Delete js directory

--- js/autoTypingAnimation.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function() {
-    $('.about-info-inner').typed({
-      strings: [
-        "<span>Hi, I am Saif Manihar</span> Completed Graduation in IT with skills Enterprise Java , MYSQL , C# , MongoDB , C++ and Honing it more. An adaptable and responsible Student seeking an entry into IT Sector. An excellent academic record with active participation in activities shows myself to be selfmotivated, organized and capable of working under pressure. Science studies gave me a logical mind and    problem solving attitude. Eagerly looking forward to learn and grow."
-      ],
-      typeSpeed: 7,
-      contentType: 'html'
-    });
-  }, 100);
